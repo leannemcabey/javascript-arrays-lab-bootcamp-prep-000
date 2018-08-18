f0177002be3204584d325ad3d527475568c2e815
Leanne Cabey <leanne.m.cabey@gmail.com>
2018-08-18 16:53:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function destructivelyPrependKitten(name) {
 function destructivelyRemoveLastKitten() {
   kittens.pop(name)
 }
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(name)
+}
